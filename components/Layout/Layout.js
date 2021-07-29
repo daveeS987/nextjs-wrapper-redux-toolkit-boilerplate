6ed85ab82b48cd901d5e73167ba61d2a1a5544c4
Davee Sok <daveesok@gmail.com>
2021-07-29 16:41:25 -0500

    initializing with some files

---
+++ components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+
+function Layout() {
+  return (
+    <>
+      <Head>
+        <title>A NextJS BoilerPlate</title>
+        <meta
+          name="Name of Project Goes Here"
+          content="Description of Website content goes here"
+        />
+      </Head>
+    </>
+  );
+}
+
+export default Layout;
